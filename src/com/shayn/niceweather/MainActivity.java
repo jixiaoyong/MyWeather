92d0a621fbc525e0fcbf07bf2c943a394fe60118
jixiaoyong <jixiaoyong1995@mail.com>
2016-04-13 15:31:25 +0800

    test

--- src/com/shayn/niceweather/MainActivity.java
+++ src/com/shayn/niceweather/MainActivity.java
@@ -14,9 +14,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
